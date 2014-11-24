397142eb98b4193ffe0929acd29d8e93ac673bc9
Sam Morrison <sorrison@gmail.com>
2014-11-24 14:17:41 +1100

    Created nectar cmd class and added rm_bdm method
    Change-Id: Iebaa3d50adb289105dcca955166185b5d6437f2e

--- nova/cmd/manage.py
+++ nova/cmd/manage.py
@@ -70,6 +70,8 @@ import six
 
 from nova.api.ec2 import ec2utils
 from nova import availability_zones
+
+from nova.cmd import nectar
 from nova import config
 from nova import context
 from nova import db
@@ -1321,6 +1323,7 @@ CATEGORIES = {
     'shell': ShellCommands,
     'vm': VmCommands,
     'vpn': VpnCommands,
+    'nectar': nectar.NectarCommands,
 }
 
 
