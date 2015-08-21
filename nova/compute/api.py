fe7c9d689c3ec1fe3eceec836a6b86bc511730e7
Mike Dorman <mdorman@godaddy.com>
2015-08-20 22:46:07 -0700

    Cellsv1: Support attach/detach interface events
    Adds support under cells v1 for interface attach/detach events.
    
    This is an updated patch for Liberty, which should supercede the
    patch at https://review.openstack.org/#/c/184155/
    
    Change-Id: I316f0339769e0ebc19fa4fcf3ee5691fb030b2ae
    Co-Authored-By: Sam Morrison <sorrison@gmail.com>
    Co-Authored-By: Mathieu Gagné <mgagne@iweb.com>
    Closes-Bug: 1456381

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -3835,6 +3835,26 @@ class API(base.Base):
                 self.volume_api.roll_detaching(context, old_volume['id'])
                 self.volume_api.unreserve_volume(context, new_volume['id'])
 
+    def _attach_interface(self, context, instance, network_id, port_id,
+                          requested_ip):
+        """Use hotplug to add a network adapter to an instance.
+
+        This method is separated to make it possible for cells version
+        to override it.
+        """
+        return self.compute_rpcapi.attach_interface(context,
+            instance=instance, network_id=network_id, port_id=port_id,
+            requested_ip=requested_ip)
+
+    def _detach_interface(self, context, instance, port_id):
+        """Detach a network adapter from an instance.
+
+        This method is separated to make it possible for cells version
+        to override it.
+        """
+        self.compute_rpcapi.detach_interface(context, instance=instance,
+            port_id=port_id)
+
     @check_instance_lock
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.PAUSED,
                                     vm_states.STOPPED],
@@ -3842,9 +3862,8 @@ class API(base.Base):
     def attach_interface(self, context, instance, network_id, port_id,
                          requested_ip):
         """Use hotplug to add an network adapter to an instance."""
-        return self.compute_rpcapi.attach_interface(context,
-            instance=instance, network_id=network_id, port_id=port_id,
-            requested_ip=requested_ip)
+        return self._attach_interface(context, instance=instance,
+            network_id=network_id, port_id=port_id, requested_ip=requested_ip)
 
     @check_instance_lock
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.PAUSED,
@@ -3852,8 +3871,7 @@ class API(base.Base):
                           task_state=[None])
     def detach_interface(self, context, instance, port_id):
         """Detach an network adapter from an instance."""
-        self.compute_rpcapi.detach_interface(context, instance=instance,
-            port_id=port_id)
+        self._detach_interface(context, instance=instance, port_id=port_id)
 
     def get_instance_metadata(self, context, instance):
         """Get all metadata associated with an instance."""
