305beefb3a65e5c8638cd55707bfff42854e07c3
Sam Morrison <sorrison@gmail.com>
2015-10-16 13:49:39 +1100

    Use security group api in EC2
    Change-Id: I6e17f29e12fdd87167463086a5ffdd9dd32206cd

--- nova/api/ec2/cloud.py
+++ nova/api/ec2/cloud.py
@@ -38,6 +38,7 @@ from nova.cloudpipe import pipelib
 from nova import compute
 from nova.compute import api as compute_api
 from nova.compute import vm_states
+from nova import context
 from nova import exception
 from nova.i18n import _
 from nova.i18n import _LI
@@ -1150,12 +1151,14 @@ class CloudController(object):
         flavor = instance.get_flavor()
         result['instanceType'] = flavor.name
 
-    @staticmethod
-    def _format_group_set(instance, result):
+    def _format_group_set(self, instance, result):
+        ctxt = context.get_admin_context()
         security_group_names = []
-        if instance.get('security_groups'):
-            for security_group in instance.security_groups:
-                security_group_names.append(security_group['name'])
+        security_groups = self.security_group_api.get_instance_security_groups(
+            ctxt, instance.uuid)
+
+        for security_group in security_groups:
+            security_group_names.append(security_group['name'])
         result['groupSet'] = utils.convert_to_list_dict(
             security_group_names, 'groupId')
 
