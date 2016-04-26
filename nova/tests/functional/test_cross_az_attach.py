2be470cf302d08f70d4eba55ec47ec594d0f7bee
Sam Morrison <sorrison@gmail.com>
2016-04-27 08:46:10 +1000

    Nectarism: Allow cross attach if volume is a 'parent'
    Also remove the upstream tests when no az specified
    
    Change-Id: Ie13f5303af5020d011a8d84baf6f1e5c6f4dcda3

--- nova/tests/functional/test_cross_az_attach.py
+++ nova/tests/functional/test_cross_az_attach.py
@@ -10,11 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import six
-
 from nova import test
 from nova.tests import fixtures as nova_fixtures
-from nova.tests.functional.api import client as api_client
 from nova.tests.functional import fixtures as func_fixtures
 from nova.tests.functional import integrated_helpers
 from nova.tests.unit.image import fake as fake_image
@@ -45,65 +42,6 @@ class CrossAZAttachTestCase(test.TestCase,
         self.start_service('scheduler')
         # Start one compute service.
         self.start_service('compute', host='host1')
-
-    def test_cross_az_attach_false_boot_from_volume_no_az_specified(self):
-        """Tests the scenario where [cinder]/cross_az_attach=False and the
-        server is created with a pre-existing volume but the server create
-        request does not specify an AZ nor is [DEFAULT]/default_schedule_zone
-        set.
-        """
-        self.flags(cross_az_attach=False, group='cinder')
-        server = self._build_minimal_create_server_request(
-            self.api,
-            'test_cross_az_attach_false_boot_from_volume_no_az_specified')
-        del server['imageRef']  # Do not need imageRef for boot from volume.
-        server['block_device_mapping_v2'] = [{
-            'source_type': 'volume',
-            'destination_type': 'volume',
-            'boot_index': 0,
-            'uuid': nova_fixtures.CinderFixture.IMAGE_BACKED_VOL
-        }]
-        # FIXME(mriedem): This is bug 1694844 where the user creates the server
-        # without specifying an AZ and there is no default_schedule_zone set
-        # and the cross_az_attach check fails because the volume's availability
-        # zone shows up as "us-central-1" and None != "us-central-1" so the API
-        # thinks the cross_az_attach=False setting was violated.
-        ex = self.assertRaises(api_client.OpenStackApiException,
-                               self.api.post_server, {'server': server})
-        self.assertEqual(400, ex.response.status_code)
-        self.assertIn('are not in the same availability_zone',
-                      six.text_type(ex))
-
-    def test_cross_az_attach_false_data_volume_no_az_specified(self):
-        """Tests the scenario where [cinder]/cross_az_attach=False and the
-        server is created with a pre-existing volume as a non-boot data volume
-        but the server create request does not specify an AZ nor is
-        [DEFAULT]/default_schedule_zone set.
-        """
-        self.flags(cross_az_attach=False, group='cinder')
-        server = self._build_minimal_create_server_request(
-            self.api,
-            'test_cross_az_attach_false_data_volume_no_az_specified')
-        # Note that we use the legacy block_device_mapping parameter rather
-        # than block_device_mapping_v2 because that will create an implicit
-        # source_type=image, destination_type=local, boot_index=0,
-        # uuid=$imageRef which is used as the root BDM and allows our
-        # non-boot data volume to be attached during server create. Otherwise
-        # we get InvalidBDMBootSequence.
-        server['block_device_mapping'] = [{
-            # This is a non-bootable volume in the CinderFixture.
-            'volume_id': nova_fixtures.CinderFixture.SWAP_OLD_VOL
-        }]
-        # FIXME(mriedem): This is bug 1694844 where the user creates the server
-        # without specifying an AZ and there is no default_schedule_zone set
-        # and the cross_az_attach check fails because the volume's availability
-        # zone shows up as "us-central-1" and None != "us-central-1" so the API
-        # thinks the cross_az_attach=False setting was violated.
-        ex = self.assertRaises(api_client.OpenStackApiException,
-                               self.api.post_server, {'server': server})
-        self.assertEqual(400, ex.response.status_code)
-        self.assertIn('are not in the same availability_zone',
-                      six.text_type(ex))
 
     def test_cross_az_attach_false_boot_from_volume_default_zone_match(self):
         """Tests the scenario where [cinder]/cross_az_attach=False and the
