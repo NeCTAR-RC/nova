2c1ad7bbc4714a143ad90335899ca5743f129539
Sam Morrison <sorrison@gmail.com>
2016-11-15 15:33:16 +1100

    Mitaka compat: drop check for detach_volume instance_uuid
    Change-Id: I98186bcedd458ed5307e80bbbbc871c054da522c

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -3125,10 +3125,14 @@ class API(base.Base):
         if volume['attach_status'] == 'detached':
             msg = _("Volume must be attached in order to detach.")
             raise exception.InvalidVolume(reason=msg)
-        # The caller likely got the instance from volume['instance_uuid']
-        # in the first place, but let's sanity check.
-        if volume['instance_uuid'] != instance.uuid:
+        try:
+            vol_instance_uuid = volume['instance_uuid']
+        except KeyError:
+            # Mitaka calls don't include instance_uuid
+            vol_instance_uuid = None
+        if volume_instance_uuid and volume_instance_uuid != instance.uuid:
             raise exception.VolumeUnattached(volume_id=volume['id'])
+
         self._detach_volume(context, instance, volume)
 
     @wrap_check_policy
