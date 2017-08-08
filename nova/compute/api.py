7aa9ba98cdd56b63f50d58d7d8ac2049d52e664c
Sam Morrison <sorrison@gmail.com>
2017-08-08 15:16:16 +1000

    CellsV1: Don't validate hosts when adding/removing aggregates
    Change-Id: I73accb4fb21c85b2e35feca5e17a7fad11f43b33

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -4267,7 +4267,9 @@ class AggregateAPI(base.Base):
                                                     "addhost.start",
                                                     aggregate_payload)
         # validates the host; ComputeHostNotFound is raised if invalid
-        objects.Service.get_by_compute_host(context, host_name)
+        # Disabled for cellsV1
+        if not CONF.cells.enable:
+            objects.Service.get_by_compute_host(context, host_name)
 
         aggregate = objects.Aggregate.get_by_id(context, aggregate_id)
         self.is_safe_to_update_az(context, aggregate.metadata,
@@ -4294,7 +4296,10 @@ class AggregateAPI(base.Base):
                                                     "removehost.start",
                                                     aggregate_payload)
         # validates the host; ComputeHostNotFound is raised if invalid
-        objects.Service.get_by_compute_host(context, host_name)
+        # Disabled for cellsV1
+        if not CONF.cells.enable:
+            objects.Service.get_by_compute_host(context, host_name)
+
         aggregate = objects.Aggregate.get_by_id(context, aggregate_id)
         aggregate.delete_host(host_name)
         self.scheduler_client.update_aggregates(context, [aggregate])
