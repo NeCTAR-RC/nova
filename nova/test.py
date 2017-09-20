87e68ece5b637b94a5cd12b1eb2b6bc8054d2632
Shahaan Ayyub <shahaan@gmail.com>
2017-09-20 10:44:11 -0400

    Support qemu >= 2.10
    Qemu 2.10 added the requirement of a --force-share flag to qemu-img
    info when reading information about a disk that is in use by a
    guest. We do this a lot in Nova for operations like gathering
    information before live migration.
    
    Up until this point all qemu/libvirt version matching has been solely
    inside the libvirt driver, however all the image manip code was moved
    out to nova.virt.images. We need the version of QEMU available there.
    
    This does it by initializing that version on driver init host. The net
    effect is also that broken libvirt connections are figured out
    earlier, as there is an active probe for this value.
    
    Change-Id: Iae2962bb86100f03fd3ad9aac3767da876291e74
    Closes-Bug: #1718295
    (cherry picked from commit 807579755c4a116309eca5b2bcdbab9d1f393bab)

--- nova/test.py
+++ nova/test.py
@@ -54,6 +54,7 @@ from nova.tests import fixtures as nova_fixtures
 from nova.tests.unit import conf_fixture
 from nova.tests.unit import policy_fixture
 from nova import utils
+from nova.virt import images
 
 
 CONF = cfg.CONF
@@ -239,6 +240,11 @@ class TestCase(testtools.TestCase):
         # nova.utils._IS_NEUTRON.  We set it to None to avoid any
         # caching of that value.
         utils._IS_NEUTRON = None
+
+        # Reset the traits sync flag
+        objects.resource_provider._TRAITS_SYNCED = False
+        # Reset the global QEMU version flag.
+        images.QEMU_VERSION = None
 
         mox_fixture = self.useFixture(moxstubout.MoxStubout())
         self.mox = mox_fixture.mox
