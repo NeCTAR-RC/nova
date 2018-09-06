e79b9afb5b8e38522c54e46952200357bc44f8a7
Sam Morrison <sorrison@gmail.com>
2018-09-06 15:50:13 +1000

    CellsV1: Service and Hypervisor API hacks
    Change-Id: I002c5ba6583411f3cefcccf2b4f084a9c5f77a9d

--- nova/api/openstack/compute/services.py
+++ nova/api/openstack/compute/services.py
@@ -300,10 +300,11 @@ class ServiceController(wsgi.Controller):
         perform a given update, which is defined in the body of the request.
         """
         service_id = id
-        # Validate that the service ID is a UUID.
-        if not uuidutils.is_uuid_like(service_id):
-            msg = _('Invalid uuid %s') % service_id
-            raise webob.exc.HTTPBadRequest(explanation=msg)
+        if not CONF.cells.enable:
+            # Validate that the service ID is a UUID.
+            if not uuidutils.is_uuid_like(service_id):
+                msg = _('Invalid uuid %s') % service_id
+                raise webob.exc.HTTPBadRequest(explanation=msg)
 
         # Validate the request context against the policy.
         context = req.environ['nova.context']
@@ -333,6 +334,15 @@ class ServiceController(wsgi.Controller):
                    {'binary': service.binary})
             raise webob.exc.HTTPBadRequest(explanation=msg)
 
+        if CONF.cells.enable:
+            if body['status'] == 'disabled':
+                action = 'disable'
+            elif body['status'] == 'enabled':
+                action = 'enable'
+            body['host'] = service.host
+            body['binary'] = service['binary']
+            actions = self.actions
+            return self._perform_action(req, action, body, actions)
         # Now determine the update to perform based on the body. We are
         # intentionally not using _perform_action or the other old-style
         # action functions.
