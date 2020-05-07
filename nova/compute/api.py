dcc46359057fd14c7e6e151d0496d424c4f966bf
Rocky <shi.yan@unimelb.edu.au>
2020-05-08 02:38:51 +1000

    Fix aggregate placement sync issue
    Use node name to sync the nova aggregate to placement resource provider
    aggregate.
    
    Change-Id: I08f2f07c0c695d1dd5b4fa72d0155e02b1872b0a
    closes-bug: #1877229

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -5705,8 +5705,10 @@ class AggregateAPI(base.Base):
         aggregate.add_host(host_name)
         self.query_client.update_aggregates(context, [aggregate])
         try:
+            nodes = objects.ComputeNodeList.get_all_by_host(context, host_name)
+            node_name = nodes[0].hypervisor_hostname
             self.placement_client.aggregate_add_host(
-                context, aggregate.uuid, host_name=host_name)
+                context, aggregate.uuid, host_name=node_name)
         except exception.PlacementAPIConnectFailure:
             # NOTE(jaypipes): Rocky should be able to tolerate the nova-api
             # service not communicating with the Placement API, so just log a
@@ -5771,8 +5773,10 @@ class AggregateAPI(base.Base):
         aggregate.delete_host(host_name)
         self.query_client.update_aggregates(context, [aggregate])
         try:
+            nodes = objects.ComputeNodeList.get_all_by_host(context, host_name)
+            node_name = nodes[0].hypervisor_hostname
             self.placement_client.aggregate_remove_host(
-                context, aggregate.uuid, host_name)
+                context, aggregate.uuid, node_name)
         except exception.PlacementAPIConnectFailure:
             # NOTE(jaypipes): Rocky should be able to tolerate the nova-api
             # service not communicating with the Placement API, so just log a
