8c530f4a6f3247a7963c8a454263d9167ced204f
Dan Smith <dansmith@redhat.com>
2024-04-17 07:06:13 -0700

    Check images with format_inspector for safety
    It has been asserted that we should not be calling qemu-img info
    on untrusted files. That means we need to know if they have a
    backing_file, data_file or other unsafe configuration *before* we use
    qemu-img to probe or convert them.
    
    This grafts glance's format_inspector module into nova/images so we
    can use it to check the file early for safety. The expectation is that
    this will be moved to oslo.utils (or something) later and thus we will
    just delete the file from nova and change our import when that happens.
    
    NOTE: This includes whitespace changes from the glance version of
    format_inspector.py because of autopep8 demands.
    
    Conflicts:
    	nova/conf/workarounds.py
    
    Change-Id: Iaefbe41b4c4bf0cf95d8f621653fdf65062aaa59
    Closes-Bug: #2059809
    (cherry picked from commit 9cdce715945619fc851ab3f43c97fab4bae4e35a)
    (cherry picked from commit f07fa55fd86726eeafcd4c0c687bc49dd4df9f4c)
    (cherry picked from commit 0acf5ee7b5dfb6ff0f9a9745f5ad2a0ed2bf65bf)
    (cherry picked from commit 67e5376dd64407f5aaf1ea5f8c896e356064a2c9)
    (cherry picked from commit 003e88ef38cd4c477ddf7d92bfff60ffb5907cf8)
    (cherry picked from commit 556cc424764cddd8bc5af8ad3ebc03b23fabd1c3)

--- nova/conf/workarounds.py
+++ nova/conf/workarounds.py
@@ -389,6 +389,16 @@ the compute nodes start up, but control services need to be online first.
                 default=2,
                 help='Address space limit in gigabytes to introspect the '
                 'image'),
+    cfg.BoolOpt(
+        'disable_deep_image_inspection',
+        default=False,
+        help="""
+This disables the additional deep image inspection that the compute node does
+when downloading from glance. This includes backing-file, data-file, and
+known-features detection *before* passing the image to qemu-img. Generally,
+this inspection should be enabled for maximum safety, but this workaround
+option allows disabling it if there is a compatibility concern.
+"""),
 ]
 
 
